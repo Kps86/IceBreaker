d3bc31456b5078e98c062c07ca0d63d649c8d4d3
kps <massiotedge@gmail.com>
2026-03-08 16:29:12 +0000

    Added prompt and GPT summarize ops

--- main.py
+++ main.py
@@ -1,9 +1,138 @@
 from dotenv import load_dotenv
+from langchain_core.prompts  import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
 def main():
     print("Hello from icebreaker!")
+
+Details = """ "Abdul Kalam" redirects here. For the member of Uttar Pradesh legislative assembly, see Abdul Kalam (Varanasi politician).
+A. P. J. Abdul Kalam
+
+Official portrait, c. 2002
+President of India
+In office
+25 July 2002 – 25 July 2007
+Prime Minister	Atal Bihari Vajpayee
+Manmohan Singh
+Vice President	Krishan Kant
+Bhairon Singh Shekhawat
+Preceded by	K. R. Narayanan
+Succeeded by	Pratibha Patil
+Principal Scientific Adviser to the Government of India
+In office
+November 1999 – November 2001
+President	K. R. Narayanan
+Prime Minister	Atal Bihari Vajpayee
+Preceded by	Office established
+Succeeded by	Rajagopala Chidambaram
+Director General of Defence Research and Development Organisation
+In office
+1992–1999
+Preceded by	Raja Ramanna
+Succeeded by	Vasudev Kalkunte Aatre
+Personal details
+Born	15 October 1931
+Rameswaram, Madras Presidency, British India
+(present day Tamil Nadu, India)
+Died	27 July 2015 (aged 83)
+Shillong, Meghalaya, India
+Resting place	Dr. A. P. J. Abdul Kalam Memorial
+Party	Independent[1]
+Alma mater	
+St. Joseph's College (BEng)
+Madras Institute of Technology (MEng)
+Profession	
+Aerospace scientistauthor
+Awards	List of awards and honours
+Notable work(s)	
+Wings of Fire
+India 2020
+Ignited Minds
+Indomitable Spirit
+Transcendence: My Spiritual Experiences with Pramukh Swamiji
+Scientific career
+Honours	Padma Bhushan (1981)
+Padma Vibhushan (1990)
+Bharat Ratna (1997)
+Fields	Aerospace engineering
+Institutions	
+Defence Research and Development Organisation
+Indian Space Research Organisation
+Website	A. P. J. Abdul Kalam Centre
+Signature
+
+Avul Pakir Jainulabdeen Abdul Kalam (/ˈʌbdʊl kəˈlɑːm/ ⓘ UB-duul kə-LAHM; 15 October 1931 – 27 July 2015) was an Indian aerospace scientist and statesman who served as the president of India from 2002 to 2007.
+
+Born and raised in a Muslim family in Rameswaram, Tamil Nadu, Kalam studied physics and aerospace engineering. He spent the next four decades as a scientist and science administrator, mainly at the Defence Research and Development Organisation (DRDO) and Indian Space Research Organisation (ISRO) and was intimately involved in India's civilian space programme and military missile development efforts. He was known as the "Missile Man of India" for his work on the development of ballistic missile and launch vehicle technology. He also played a pivotal organisational, technical, and political role in Pokhran-II nuclear tests in 1998, India's second such test after the first test in 1974.
+
+Kalam was elected as the president of India in 2002 with the support of both the ruling Bharatiya Janata Party and the then-opposition Indian National Congress. He was widely referred to as the "People's President". He engaged in teaching, writing and public service after his presidency. He was a recipient of several awards, including the Bharat Ratna, India's highest civilian honour.
+
+While delivering a lecture at IIM Shillong, Kalam collapsed and died from an apparent cardiac arrest on 27 July 2015, aged 83. Thousands attended the funeral ceremony held in his hometown of Rameswaram, where he was buried with full state honours. A memorial was inaugurated near his home town in 2017.
+
+Early life and education
+Avul Pakir Jainulabdeen Abdul Kalam was born on 15 October 1931 to a Tamil Muslim family in the pilgrimage center of Rameswaram on Pamban Island, Madras Presidency (now in the Indian state of Tamil Nadu).[2][3] His father, Jainulabdeen Marakayar, was a boat owner and imam of a local mosque,[4] and his mother, Ashiamma, was a housewife.[5][6] His father owned a boat that ferried Hindu pilgrims between Rameswaram and Dhanushkodi.[7][8]
+
+
+Kalam's birthplace in Rameswaram, Tamil Nadu
+	
+This article is part of
+a series about
+A. P. J. Abdul Kalam
+Presidency
+(2002–2007)
+Presidential elections 2002
+Awards and honoursEponymsDeath and state funeralMemorialWorld Students' Day
+Books and publications
+
+Wings of FireIndia 2020Ignited MindsTarget 3 BillionTranscendenceIndomitable Spirit
+Associated projects
+Integrated Guided Missile Development ProgrammePokhran-IIProject DevilProject ValiantPolar Satellite Launch VehicleSatellite Launch VehicleSmiling Buddha
+Gallery: Picture, Sound, Video
+vte
+Kalam was the youngest of four brothers and a sister in the family.[9][10][11] His ancestors had been wealthy Marakayar traders and landowners, with numerous properties and large tracts of land. Marakayar are a Muslim ethnic group found in coastal Tamil Nadu and Sri Lanka who claim descent from Arab traders and local women. The family business had involved trading goods and transporting passengers between the Indian mainland and the Pamban Island and to and from Sri Lanka. With the opening of the Pamban Bridge connecting Pamban Island to mainland India in 1914, the businesses failed. As a result, apart from the ancestral home, the other family fortune and properties were lost by the 1920s, and the family was poverty-stricken by the time Kalam was born. As a young boy, he delivered newspapers to support the family's meager income.[12][13][14]
+
+In his school years, Kalam got average grades but was described by his teachers as a bright and hardworking student with a strong desire to learn. He spent hours learning Mathematics.[14] He did his schooling at Schwartz Higher Secondary School in Ramanathapuram.[15] He then graduated in Physics from St. Joseph's College, Tiruchirappalli, in 1954.[16]
+
+Kalam moved to Madras in 1955 to study aerospace engineering at the Madras Institute of Technology.[3] While he was working on a class project, the Dean of the institution was dissatisfied with his lack of progress and threatened to revoke his scholarship unless the project was finished within the next three days. Kalam met the deadline, impressing the Dean, who later said to him, "I was putting you under stress and asking you to meet a difficult deadline."[17] Later, he narrowly missed out on his dream of becoming a fighter pilot, as he placed ninth in qualifiers, and only eight positions were available in the Indian Air Force.[18]
+
+Career as a scientist
+This was my first stage, in which I learnt leadership from three great teachers—Dr Vikram Sarabhai, Prof Satish Dhawan and Dr Brahm Prakash. This was the time of learning and acquisition of knowledge for me.
+
+Abdul Kalam[19]
+After graduating from the Madras Institute of Technology in 1960, Kalam became a member of the Defence Research & Development Service and joined the Aeronautical Development Establishment of the Defence Research and Development Organisation (DRDO) as a scientist. During his early career, he was involved in the design of small hovercraft, and remained unconvinced by his choice of a job at DRDO.[20] Later, he joined the Indian National Committee for Space Research, working under renowned space scientist Vikram Sarabhai.[3] He was interviewed and recruited into Indian Space Research Organisation (ISRO) by H. G. S. Murthy, the first director of the Thumba Equatorial Rocket Launching Station.[21]
+
+In 1969, Kalam transferred to ISRO where he became the project director of India's first satellite launch vehicle (SLV) which successfully deployed the Rohini satellite in near-earth orbit in July 1980. He had earlier started work on an expandable rocket project independently at DRDO in 1965.[22] In 1969, Kalam received the approval from the Government of India to expand the programme to include more engineers.[19] In 1963–64, he visited NASA's Langley Research Center in Hampton, Goddard Space Flight Center in Greenbelt, and Wallops Flight Facility.[2][23] Since the late 1970s, Kalam was part of the effort to develop the SLV-3 and Polar Satellite Launch Vehicle (PSLV), both of which were successful.[24][25]
+
+In May 1974, Kalam was invited by Raja Ramanna to witness the country's first nuclear test Smiling Buddha as the representative of Terminal Ballistics Research Laboratory, even though he was officially not part of the project.[26] In the 1970s, Kalam directed two projects, Project Devil and Project Valiant, which sought to develop ballistic missiles using the technology from the successful SLV programme. Despite the disapproval of the union cabinet, then Prime Minister Indira Gandhi allotted funds for these aerospace projects under Kalam's directorship through her discretionary powers. Kalam also played a major role in convincing the cabinet to conceal the true nature of these classified projects. His research and leadership brought him recognition in the 1980s, which prompted the government to initiate an advanced missile programme under his directorship.[27]
+
+Kalam worked with metallurgist V. S. R. Arunachalam, who was then scientific adviser to the Defence Minister, on the suggestion by the then Defence Minister R. Venkataraman on the simultaneous development of a quiver of missiles instead of taking planned missiles one after another.[28] Venkatraman was instrumental in getting the cabinet approval for allocating ₹3.88 billion (equivalent to ₹66 billion or US$780 million in 2023) for the project titled Integrated Guided Missile Development Programme (IGMDP) and appointed Kalam as its chief executive.[28] Kalam played a major role in the development of missiles including Agni, an intermediate range ballistic missile and Prithvi, the tactical surface-to-surface missile, despite inflated costs and time overruns.[28][29] He was known as the "Missile Man of India" for his work on the development of ballistic missile and launch vehicle technology.[30][31][32]
+
+Kalam served as the chief scientific adviser to the prime minister and secretary of the DRDO from July 1992 to December 1999. He played a key organisational, political and technical role in the Pokhran-II nuclear tests conducted in May 1998.[33] Along with Rajagopala Chidambaram, he served as the chief project coordinator for the tests.[2][34] Media coverage of Kalam during this period made him the country's best known nuclear scientist.[35] However, the director of the site test, K. Santhanam, said that the thermonuclear bomb had been a "fizzle" and criticised Kalam for issuing an incorrect report.[36] The claim was refuted and rejected by Kalam and Chidambaram.[37]
+
+In 1998, Kalam worked with cardiologist Bhupathiraju Somaraju and developed a low cost coronary stent, named the "Kalam-Raju stent".[38][39] In 2012, the duo designed a tablet computer named the "Kalam-Raju tablet" for usage by healthcare workers in rural areas.[40]"""
+
+
+summary_template = """ given the {information} about  aperson I want to create 
+1. Short Summry
+2. Two interresting facts about that person
+"""
+
+summary_prompt_template = PromptTemplate(
+    input_variables=["information"], template=summary_template
+)
+
+llm = ChatOpenAI(temperature=0,model="gpt-5")
+#llm = ChatOllama(temperature=0,model="gemma3:270m")
+
+chain = summary_prompt_template | llm
+
+response = chain.invoke(input={"information" : Details})
+
+print(response.content)
+
 
 
 if __name__ == "__main__":
